498fa21bb0559a841340496eeb316422d9d2c7e7
Jamar Green <jamargreen22@gmail.com>
2021-12-20 19:33:20 -0600

    initial commit

---
+++ ChallengesWithTestsMark8/ChallengesSet01Base.cs
@@ -0,0 +1,19 @@
+﻿namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet01Base
+    {
+
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            int lowestValue = 100;
+            if (number1 < lowestValue)
+            {
+                lowestValue = number1;
+            }
+            if (number2 < lowestValue)
+            {
+                lowestValue = number2;
+            }
+        }
+    }
+}
